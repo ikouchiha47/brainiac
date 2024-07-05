e120e0c658ffdd55011cfe736c022a7b293f29bc
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-07-05 12:36:08 +0530

    chore: bitchass parser blowing up

--- bettledb/src/parser.zig
+++ bettledb/src/parser.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const testing = std.testing;
+
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
 
@@ -15,10 +17,37 @@ const ASTNodeType = enum {
     Literal,
 };
 
+const ColumnType = enum {
+    Integer,
+    Varchar,
+    Text,
+    Decimal,
+    Datetime,
+};
+
+const ColumnConstraint = enum {
+    PrimaryKey,
+    NotNull,
+    Unique,
+};
+
+const data_pairs = [_]struct { []const u8, ColumnType }{
+    .{ "INTEGER", .Integer },
+    .{ "VARCHAR", .Varchar },
+    .{ "TEXT", .Text },
+    .{ "DECIMAL", .Decimal },
+    .{ "DATETIME", .Datetime },
+};
+const constraint_pairs = [_]struct { []const u8, ColumnConstraint }{
+    .{ "PRIMARY", .PrimaryKey },
+    .{ "NOT", .NotNull },
+    .{ "UNIQUE", .Unique },
+};
+
 pub const ASTNode = struct {
     type: ASTNodeType,
-    value: ?[]const u8 = null,
     children: ArrayList(ASTNode),
+    value: ?[]const u8 = null,
 
     fn init(allocator: Allocator, nodeType: ASTNodeType) !ASTNode {
         return ASTNode{
@@ -27,7 +56,7 @@ pub const ASTNode = struct {
         };
     }
 
-    fn deinit(self: *ASTNode) void {
+    fn deinit(self: *const ASTNode) void {
         for (self.children.items) |*child| {
             child.deinit();
         }
@@ -40,14 +69,35 @@ const Parser = struct {
     lexer: *t.Lexer,
     current_token: t.Token,
 
-    fn init(allocator: Allocator, lexer: *t.Lexer) Parser {
+    data_type_map: std.StringHashMap(ColumnType),
+    constraint_map: std.StringHashMap(ColumnConstraint),
+
+    fn init(allocator: Allocator, lexer: *t.Lexer) !Parser {
+        var data_type_map = std.StringHashMap(ColumnType).init(allocator);
+        var constraint_map = std.StringHashMap(ColumnConstraint).init(allocator);
+
+        for (data_pairs) |pair| {
+            try data_type_map.put(pair[0], pair[1]);
+        }
+
+        for (constraint_pairs) |pair| {
+            try constraint_map.put(pair[0], pair[1]);
+        }
+
         var parser = Parser{
             .allocator = allocator,
             .lexer = lexer,
             .current_token = undefined,
+            .data_type_map = data_type_map,
+            .constraint_map = constraint_map,
         };
         parser.nextToken();
         return parser;
+    }
+
+    fn deinit(self: *Parser) void {
+        self.data_type_map.deinit();
+        self.constraint_map.deinit();
     }
 
     fn nextToken(self: *Parser) void {
@@ -82,10 +132,13 @@ const Parser = struct {
         while (self.current_token.type != .RightParen) {
             const columnDef = try self.parseColumnDef();
             try node.children.append(columnDef);
+
             if (self.current_token.type == .Comma) {
                 self.nextToken();
             } else break;
         }
+
+        std.debug.print("token {}\n", .{self.current_token});
 
         try self.consumeToken(.RightParen);
         try self.consumeToken(.Semicolon);
@@ -98,11 +151,67 @@ const Parser = struct {
         errdefer node.deinit();
 
         node.value = try self.consumeIdentifier();
-        _ = try self.consumeIdentifier(); // Data type
-        // Here you would typically store the data type and any constraints
-        // For simplicity, we're not storing them in this example
+
+        const dataType = try self.parseDataType();
+        var typeNode = try ASTNode.init(self.allocator, .Literal);
+        typeNode.value = @tagName(dataType);
+
+        try node.children.append(typeNode);
+
+        // parse constraint
+        while (self.current_token.type == .Keyword) {
+            const constraint = try self.parseConstraints();
+            var constraintNode = try ASTNode.init(self.allocator, .Literal);
+
+            constraintNode.value = @tagName(constraint);
+            try node.children.append(constraintNode);
+        }
 
         return node;
+    }
+
+    fn parseDataType(self: *Parser) !ColumnType {
+        var buf: [1024]u8 = undefined;
+        const data_type = try self.consumeIdentifier();
+        const upperDataType = std.ascii.upperString(&buf, data_type);
+
+        if (self.data_type_map.get(upperDataType)) |columnType| {
+            switch (columnType) {
+                .Varchar => {
+                    try self.consumeToken(.LeftParen);
+                    _ = try self.consumeToken(.Number); // Ignore size for now
+                    try self.consumeToken(.RightParen);
+                },
+                .Decimal => {
+                    try self.consumeToken(.LeftParen);
+                    _ = try self.consumeToken(.Number); // Precision
+                    try self.consumeToken(.Comma);
+                    _ = try self.consumeToken(.Number); // Scale
+                    try self.consumeToken(.RightParen);
+                },
+                else => {},
+            }
+            return columnType;
+        }
+
+        return error.UnsupportedDataType;
+    }
+
+    fn parseConstraints(self: *Parser) !ColumnConstraint {
+        var buf: [1024]u8 = undefined;
+        const constraint = try self.consumeIdentifier();
+        const upperConstraint = std.ascii.upperString(&buf, constraint);
+
+        if (self.constraint_map.get(upperConstraint)) |columnConstraint| {
+            switch (columnConstraint) {
+                .PrimaryKey => try self.consumeKeyword("KEY"),
+                .NotNull => try self.consumeKeyword("NULL"),
+                .Unique => {},
+            }
+            return columnConstraint;
+        }
+
+        return error.UnsupportedConstraint;
     }
 
     fn parseInsert(self: *Parser) !ASTNode {
@@ -226,3 +335,34 @@ const Parser = struct {
         return value;
     }
 };
+
+test "Parser - CREATE TABLE statement success" {
+    const input = "CREATE TABLE users (id INT, name VARCHAR(255), age INT);";
+    var lexer = t.Lexer.init(input);
+    var parser = try Parser.init(testing.allocator, &lexer);
+    defer parser.deinit();
+
+    const ast = try parser.parse();
+    defer ast.deinit();
+
+    try testing.expectEqual(ASTNodeType.CreateTable, ast.type);
+    try testing.expectEqual(@as(usize, 4), ast.children.items.len);
+
+    // Check table name
+    const tableName = ast.children.items[0];
+    try testing.expectEqual(ASTNodeType.TableName, tableName.type);
+    try testing.expectEqualStrings("users", tableName.value.?);
+
+    // Check column definitions
+    const idColumn = ast.children.items[1];
+    try testing.expectEqual(ASTNodeType.ColumnDef, idColumn.type);
+    try testing.expectEqualStrings("id", idColumn.value.?);
+
+    const nameColumn = ast.children.items[2];
+    try testing.expectEqual(ASTNodeType.ColumnDef, nameColumn.type);
+    try testing.expectEqualStrings("name", nameColumn.value.?);
+
+    const ageColumn = ast.children.items[3];
+    try testing.expectEqual(ASTNodeType.ColumnDef, ageColumn.type);
+    try testing.expectEqualStrings("age", ageColumn.value.?);
+}
